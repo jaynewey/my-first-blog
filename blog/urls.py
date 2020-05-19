ff55177c60ef3b65008d52f78d90fc67edd0ae2c
Jay Newey <jayluxray@gmail.com>
2020-05-19 19:37:39 +0100

    Make site mobile responsive

--- blog/urls.py
+++ blog/urls.py
@@ -1,3 +1,4 @@
+from django.contrib import admin
 from django.urls import path, include 
 from . import views
 
@@ -6,7 +7,7 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
-    
+
     path('accounts/', include('django.contrib.auth.urls')),
 
 ]
