28360a8db629b1b37f8227af89e34a9201711daf
Jay Newey <jayluxray@gmail.com>
2020-05-27 22:35:08 +0100

    Add search queries to post list

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from django.db.models import Q
 from .models import Post
 from .forms import PostForm
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    search = request.GET.get('q')
+    if search and search != "All posts":
+        lookups= Q(title__contains=search) | Q(description__icontains=search) | Q(tags__icontains=search)
+        posts = Post.objects.filter(lookups).distinct()
+    else:
+        posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
